316c3a8cb916981eb68feeb48c3bcd612fc8bb6a
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-12 13:10:42 -0500

    Remove debuggy puts

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,7 @@
 class PigLatinizer
 
   def piglatinize(input_str)
-    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-    puts x
-    x
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
 
   private
