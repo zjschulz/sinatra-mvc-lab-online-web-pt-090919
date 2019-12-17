48c564cc2869744170db61450d833d1f80c595a3
Zachary Schulz <zjschulz@gmail.com>
2019-12-17 04:16:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  
+  get "/" do
+    erb :user_input
+  end
+  
+  post "/piglatinize" do
+    @pig = params[:user_phrase]
+    erb :piglatinize
+  end
+  
 end
